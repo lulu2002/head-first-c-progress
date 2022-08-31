283c1725fdbc02b57ffd98b170f87a7c80fd462c
lulu2002 <lose9749@gmail.com>
2022-08-31 23:22:39 +0800

    完成第一章

--- ch-1/card.c
+++ ch-1/card.c
@@ -3,29 +3,42 @@
 
 int main() {
     char card_name[3];
+    int count = 0;
 
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
+    do {
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
 
-    int val;
+        int val;
 
-    switch (card_name[0]) {
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-    }
+        switch (card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
 
-    if (val == 10)
-        puts("Count has gone up");
-    else if (val >= 3 && val <= 6)
-        puts("Count has gone down");
+                if (!(val >= 1 && val <= 10)) {
+                    puts("Invalid number, please correct it and type again.");
+                    continue;
+                }
+        }
+
+        if (val == 10)
+            count++;
+        else if (val >= 3 && val <= 6)
+            count--;
+
+        printf("Current count: %i\n", count);
+
+    } while (card_name[0] != 'X');
 
     return 0;
 }
